f374d83679d7898fdc5224ed1e3b64376646135f
Kishore Sohanda <kishoresohanda100@gmail.com>
2024-03-07 16:25:20 +0530

    installed express and used express.js to handle the requests.

---
+++ app.js
@@ -0,0 +1,14 @@
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
+const express = require('express');
+const bodyParser=require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.listen(3000);
