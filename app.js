5a03f4a32e3ddb6e7d2266134ff1bb2f1ef6ea6b
Kishore Sohanda <kishoresohanda100@gmail.com>
2024-03-09 14:37:14 +0530

    added html, css files to the express app to give response to any incoming request from the client, used many of the properties like express.static(), path.join() and more stuff.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path=require('path');
 const adminRoutes=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
 
@@ -7,8 +8,12 @@ const bodyParser=require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes); 
 app.use(shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
-const express=require('express');
+const path =require('path');
 
+const rootDir=require('../util/path');
+
+const express=require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.send('<h1>Express app!</h1>');
+    res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
 module.exports=router;
