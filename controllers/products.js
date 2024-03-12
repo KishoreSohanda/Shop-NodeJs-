a558439d95e39418d1a077067cfb7bb40d1cd76f
Kishore Sohanda <kishoresohanda100@gmail.com>
2024-03-12 22:13:18 +0530

    added product model to project and used file system to store the entered products from frontend.

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,17 @@
-const products = [];
+const Product=require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product.ejs', { pageTitle: 'Add Product', path: '/admin/add-product' });
 };
 
 exports.postAddProduct = (req, res, next) => {
-    // console.log(req.body);
-    products.push({ title: req.body.title });
+    const product=new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop.ejs', { prods: products, path: '/', pageTitle: 'Shop' });
+    Product.fetchAll((products)=>{
+        res.render('shop.ejs', { prods: products, path: '/', pageTitle: 'Shop' });
+    });
 };
