0ed537b561708b496cf6550ffadc3d82e24dacc7
Kishore Sohanda <kishoresohanda100@gmail.com>
2024-03-14 18:24:26 +0530

    Added model,post routes for cart and added addProduct funtionality in cart model to add a product into the cart.

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,16 @@
 const Product=require('../models/product');
+const Cart=require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products)=>{
         res.render('shop/product-list', { prods: products, path: '/products', pageTitle: 'All Products' });
+    });
+};
+
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId,product=>{
+        res.render('shop/product-detail', { product: product, path: '/products', pageTitle: product.title });
     });
 };
 
@@ -15,9 +23,21 @@ exports.getIndex=(req,res,next)=>{
 exports.getCart=(req,res,next)=>{
         res.render('shop/cart', {  path: '/cart', pageTitle: 'Your Cart' });
 };
+
+exports.postCart=(req,res,next)=>{
+    const prodId=req.body.productId;
+    Product.findById(prodId,(product)=>{
+        Cart.addProduct(prodId,product.price);
+    });
+    // console.log(prodId);
+    res.redirect('/cart');
+        // res.render('shop/cart', {  path: '/cart', pageTitle: 'Your Cart' });
+};
+
 exports.getOrders=(req,res,next)=>{
         res.render('shop/orders', {  path: '/orders', pageTitle: 'Your Orders' });
 };
+
 exports.getCheckout=(req,res,next)=>{
         res.render('shop/checkout', {  path: '/checkout', pageTitle: 'Checkout' });
 };
