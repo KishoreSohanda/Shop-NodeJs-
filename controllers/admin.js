6df97a4845800bd10b7cec88095834f48cf9da53
Kishore Sohanda <kishoresohanda100@gmail.com>
2024-03-15 15:34:51 +0530

    Added deleteProduct, getCart methods in Cart model, cart.ejs has populated data to show cart items to users on incoming request and save, deleteById methods are also added. Overall now users can add items to cart and see it under /cart. And now onwards user can delete, edit product from /adminProducts.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product=require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+    res.render('admin/edit-product', { pageTitle: 'Add Product', path: '/admin/add-product' ,editing:false});
 };
 
 exports.postAddProduct = (req, res, next) => {
@@ -9,13 +9,46 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl=req.body.imageUrl;
     const price=req.body.price;
     const description=req.body.description;
-    const product=new Product(title,imageUrl,description,price);
+    const product=new Product(null,title,imageUrl,description,price);
     product.save();
     res.redirect('/');
 };
 
+exports.getEditProduct = (req, res, next) => {
+    const editMode=req.query.edit;
+    if(!editMode){
+        console.log('Not in edit mode');
+        return res.redirect('/');
+    }
+    const prodId=req.params.productId;
+    Product.findById(prodId,(product)=>{
+        if (!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', { pageTitle: 'Edit Product', path: '/admin/edit-product',editing:editMode,product:product });
+    });
+};
+
+exports.postEditProduct = (req, res, next) => {
+    const prodId=req.body.productId;
+    const updatedTitle=req.body.title;  
+    const updatedPrice=req.body.price;  
+    const updatedImageUrl=req.body.imageUrl;  
+    const updatedDescription=req.body.description;  
+    const updatedProduct=new Product(prodId,updatedTitle,updatedImageUrl,updatedDescription,updatedPrice);
+    updatedProduct.save();
+    res.redirect('/admin/products');
+};
+
+
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products)=>{
-        res.render('admin/products', { prods: products, path: 'admin/products', pageTitle: 'Admin Products' });
+        res.render('admin/products', { prods: products, path: '/admin/products', pageTitle: 'Admin Products' });
     });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId=req.body.productId;
+    Product.deleteById(prodId);
+    res.redirect('/admin/products');
 };
