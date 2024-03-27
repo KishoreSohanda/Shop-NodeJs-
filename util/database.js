47ae9d11a331c83ade8ef20146241deba795110e
Kishore Sohanda <kishoresohanda100@gmail.com>
2024-03-27 17:21:09 +0530

    Setting up nosql mongo db connection.

--- util/database.js
+++ util/database.js
@@ -1,7 +1,30 @@
-const Sequelize=require('sequelize');
+const dbPass = require('./dbPassword');
 
-const dbPass =require('./dbPassword');
+let _db;
 
-const sequelize= new Sequelize('node-complete','root',dbPass,{dialect:'mysql',host:'localhost'});
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-module.exports=sequelize;
+const mongoConnect = (callback) => {
+    MongoClient.connect(`mongodb+srv://kishoresohanda100:${dbPass.mongodbPass}@nodeapp.ddj07fv.mongodb.net/shop?retryWrites=true&w=majority&appName=NodeApp`)
+        .then(client => {
+            _db=client.db();
+            console.log('Connected!');
+            callback();
+        })
+        .catch(err => {
+            console.log(err)
+            throw err;
+        });
+};
+
+const getDb=()=>{
+    if(_db){
+        return _db;
+    }
+    throw 'No database found!';
+};
+ 
+exports.mongoConnect=mongoConnect;
+exports.getDb=getDb;
+
